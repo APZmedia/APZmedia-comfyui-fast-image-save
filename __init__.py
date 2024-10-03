99e55e87dc793f0eec948efac87a491858a3bae2
Pablo APZmedia <me@apzmedia.com>
2024-10-03 13:56:33 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .nodes.apzmedia_fast_image_save import ImageSaveWithAlphaChannel
+
+NODE_CLASS_MAPPINGS = {
+    "APZmedia Fast image save": ImageSaveWithAlphaChannel,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "APZmedia Fast image save": "APZmedia Fast Image Save Node",
+}
